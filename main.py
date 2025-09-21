1498b27b7feab6ae7d37e8b12a0adb9f2dc2dbfa
gndmdthsyth <schwartzdavid38@gmail.com>
2025-09-20 22:14:04 -0500

    Support for custom books

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-from stats import word_count
+import sys
+from stats import word_count, char_count, get_book_text, char_sort
 def main():
-    word_count("books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_title = sys.argv[1]
+    text = get_book_text(book_title)
+    words = word_count(text)
+    sorted_chars = char_sort(char_count(text))
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_title}...")
+    print("----------- Word Count ----------")
+    print(f"Found {words} total words")
+    print("--------- Character Count -------")
+    #print(sorted_chars)
+    for char in sorted_chars:
+        print(f"{char}: {sorted_chars[char]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,16 +1,41 @@
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
-        print(file_contents)
+        return file_contents
 
-def word_count(file_path):
-    with open(file_path) as f:
-        file_contents = f.read()
-        words = file_contents.split()
-        num_words = len(words)
-        print(f"{num_words} words found in the document")
+def word_count(text):
+    words = text.split()
+    num_words = len(words)
+    return num_words
 
-def char_count(file_path):
-    with open(file_path) as f:
-        file_contents = f.read()
-        
+def char_count(text):
+    count = {}
+    
+    for c in text:
+        lowered = c.lower()
+        if lowered in count:
+            count[lowered] +=1
+        else:
+            count[lowered] = 1
+    return count
+
+def sort_on(char_count):
+    return char_count["num"]
+
+def char_sort(count):
+    sorted = []
+    sorted_dict = {}
+    for c in count:
+        num = count[c]
+        if c.isalpha():
+            sorted.append({"char": c, "num" : num})
+            #print(sorted)
+    sorted.sort(reverse = True, key=sort_on)
+    
+    for char in sorted:
+        #print(char)
+        num = char["num"]
+        c = char["char"]
+        sorted_dict[c] = num
+
+    return sorted_dict
